bb6b56bf888405da6c8f76e31c5fdec1a52dcc32
jiunnni <ya520709@gmail.com>
2017-12-07 16:27:51 +0800

    16:27

--- app/src/main/java/com/fju/mid2/Func1Activity.java
+++ app/src/main/java/com/fju/mid2/Func1Activity.java
@@ -1,13 +1,27 @@
 package com.fju.mid2;
 
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import java.util.Random;
 
 public class Func1Activity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_func1);
+
     }
+    Random random=new Random();
+    int r = random.nextInt(6)+1;
+    public void f9(View view){
+        new AlertDialog.Builder(this)
+                .setMessage(r)
+                .show();
+    }
+
 }
